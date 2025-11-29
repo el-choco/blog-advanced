c934f8f1f227dfe58c26d72be7be9942a2cfa58b
el-choco <f.sanchez@unity-mail.de>
2025-11-29 23:19:19 +0100

    Update common.php with new content

--- common.php
+++ common.php
@@ -1,17 +1,17 @@
 <?php
 
 // Define PROJECT PATH
-define('PROJECT_PATH', dirname(__FILE__).'/');
-define('APP_PATH', PROJECT_PATH+'app/');
+define('PROJECT_PATH', dirname(__FILE__) . '/');
+define('APP_PATH', PROJECT_PATH . 'app/');
 
 // Load Autoloader
-require APP_PATH+"splclassloader.class.php";
+require APP_PATH . 'splclassloader.class.php';
 $classLoader = new SplClassLoader(null, APP_PATH);
 $classLoader->setFileExtension('.class.php');
 $classLoader->register();
 
 // In debug mode, display errors
-if(Config::get_safe('debug', false)){
+if (Config::get_safe('debug', false)) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -19,16 +19,16 @@ if(Config::get_safe('debug', false)){
     // Check extensions
     $required = ['curl', 'PDO', 'pdo_mysql', 'gd', 'exif'];
     $loaded = get_loaded_extensions();
-    if($missing = array_diff($required, $loaded)){
-        die("Missing extensions, please install: ".implode(", ", $missing));
+    if ($missing = array_diff($required, $loaded)) {
+        die('Missing extensions, please install: ' . implode(', ', $missing));
     }
 }
 
 // Language
-Lang::load(empty($_GET["hl"]) ? Config::get("lang") : $_GET["hl"]);
+Lang::load(empty($_GET['hl']) ? Config::get('lang') : $_GET['hl']);
 
 // Timezone
-if(false !== ($TZ = Config::get_safe('timezone', getenv('TZ')))) {
+if (false !== ($TZ = Config::get_safe('timezone', getenv('TZ')))) {
     date_default_timezone_set($TZ);
     ini_set('date.timezone', $TZ);
 }
@@ -37,13 +37,13 @@ if(false !== ($TZ = Config::get_safe('timezone', getenv('TZ')))) {
 ini_set('session.cookie_httponly', 1);
 session_start();
 
-// Global CSRF token creation (previously missing)
+// Global CSRF token creation
 if (empty($_SESSION['token'])) {
     $_SESSION['token'] = bin2hex(random_bytes(16));
 }
 
 if (!function_exists('escape')) {
     function escape($str) {
-        return htmlspecialchars($str ??  '', ENT_QUOTES, 'UTF-8');
+        return htmlspecialchars($str ?? '', ENT_QUOTES, 'UTF-8');
     }
 }
